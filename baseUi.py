e61e1f327e25c2e6ed22baf81300d12a4d1feccc
hoani <hoani.bryson@gmail.com>
2019-11-15 17:43:57 +1300

    Progress RobCommsClient

--- baseUi.py
+++ baseUi.py
@@ -11,6 +11,69 @@ except:
   pass
 import plotCanvas
 
+class RoboCommsClient():
+  def __init__(self, client = None):
+    self.client = client
+    self.client_connecting = False
+    self.subscribers = dict()
+
+  def upkeep(self):
+    if self.client == None:
+      return
+
+    if self.client.connected == False:
+      if self.client_connecting == False:
+        self.client_connecting = True
+        try:
+          self.client.connect()
+        except:
+          pass
+        self.client_connecting = False
+    else:
+      try:
+        data = self.client.recv(1024)
+        if data != None:
+          print(data.decode('utf-8'))
+          self._process_packet(data)
+        else:
+          self.client.connected = False
+          
+      except OSError:
+        pass
+      except Exception as e:
+        print(e)
+
+  def _client_send(self, data):
+    if self.client == None:
+      return
+    else:
+      self.client.send(data)
+
+  def subscribe(self, item, callback):
+    if item in self.subscribers.keys:
+      self.subscribers[item].append(callback)
+    else:
+      self.subscribers[item] = [callback]
+
+  def _publish(self, item, payload):
+    if item in self.subscribers.keys:
+      for subscriber in self.subscribers[item]:
+        subscriber.callback(payload)
+
+  def _process_packet(self, data):
+    data = data.decode('utf-8')
+    for line in data.split('\n'):
+      items = line.split(' ')
+
+      if items[0] == "accel" or items[0] == "gyros" or items[0] == "magne":
+        if len(items) == 4:
+          accel_data = vect.Vec3(
+            float(items[1]),
+            float(items[2]),
+            float(items[3])
+          )
+          self._publish(items[0], accel_data)
+
 
 class MainWindow(QtWidgets.QMainWindow):
   def __init__(self, parent=None):
@@ -244,5 +307,24 @@ class MainWindow(QtWidgets.QMainWindow):
           self.magnetometer.x.setText("{:.2f}".format(float(items[1])))
           self.magnetometer.y.setText("{:.2f}".format(float(items[2])))
           self.magnetometer.z.setText("{:.2f}".format(float(items[3])))
-
           self.plot_mag.update_data(vect.Vec3(float(items[1]), float(items[2]), float(items[3])))
+
+
+  def data_update_accelerometers(self, data):
+    self.accelerometer.x.setText("{:.2f}".format(data.x))
+    self.accelerometer.y.setText("{:.2f}".format(data.y))
+    self.accelerometer.z.setText("{:.2f}".format(data.z))
+    self.plot_accel.update_data(vect.Vec3(data.x, data.y, data.z))
+
+  def data_update_gyroscopes(self, data):
+    self.gyroscopes.x.setText("{:.2f}".format(data.x))
+    self.gyroscopes.y.setText("{:.2f}".format(data.y))
+    self.gyroscopes.z.setText("{:.2f}".format(data.z))
+    self.plot_gyro.update_data(vect.Vec3(data.x, data.y, data.z))
+
+  def data_update_magnetometers(self, data):
+    self.magnetometer.x.setText("{:.2f}".format(data.x))
+    self.magnetometer.y.setText("{:.2f}".format(data.y))
+    self.magnetometer.z.setText("{:.2f}".format(data.z))
+    self.plot_mag.update_data(vect.Vec3(data.x, data.y, data.z))
+
