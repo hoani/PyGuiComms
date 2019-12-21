9b8f699ca3b4fb508f1d2789c47021f8bc10fb44
Hoani Bryson <hoani.bryson@gmail.com>
2019-12-21 05:46:03 +0000

    Added some compound packet support and remainder support - updated todo for completing compound packet implementation

---
+++ ahrsGui.py
@@ -0,0 +1,9 @@
+# Author: Hoani
+#
+# Manual control GUI
+from pyGuiComms.ui import uiExecute
+import sys
+
+if __name__ == '__main__':
+  sys.path.insert(0,'./external/RoBus/RoBus')
+  gui = uiExecute.UiExecute("ui/Ahrs.ui", "config/settings.json", "config/widgets.json", "config/protocol.json")
