dc731bea9329e4d7bac95640aadab4bb85eac81b
hoani <hoani.bryson@gmail.com>
2019-12-23 17:22:40 +1300

    add widgets in prep for control GUI

---
+++ controlGui.py
@@ -0,0 +1,9 @@
+# Author: Hoani
+#
+# Remote Logger GUI
+from pyGuiComms.ui import uiExecute
+import sys
+
+if __name__ == '__main__':
+  sys.path.insert(0,'./external/RoBus/RoBus')
+  gui = uiExecute.UiExecute("ui/PendulumControl.ui", "config/settings.json", "config/widgets.json", "config/protocol.json")
