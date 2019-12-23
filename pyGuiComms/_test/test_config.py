cb943d594dbef904733ab2a1c399265b266cdc73
Hoani Bryson <Hoani.Bryson@gmail.com>
2019-12-23 21:02:43 +1300

    Added all json parsing unittests

--- pyGuiComms/_test/test_config.py
+++ pyGuiComms/_test/test_config.py
@@ -1,10 +1,26 @@
 from pyGuiComms.utilities import vect
 import json
 
-class TestConfigLoads:
+class TestConfigParses:
 
   def test_protocol(self):
     filepath = 'config/protocol.json'
+    try:
+      with open(filepath, 'r') as f:
+        loaded = json.load(f)
+    except:
+      assert(False)
+
+  def test_settings(self):
+    filepath = 'config/settings.json'
+    try:
+      with open(filepath, 'r') as f:
+        loaded = json.load(f)
+    except:
+      assert(False)
+
+  def test_widgets(self):
+    filepath = 'config/widgets.json'
     try:
       with open(filepath, 'r') as f:
         loaded = json.load(f)
